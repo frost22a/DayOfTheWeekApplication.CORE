36e5490890b462468739fe9291ea472b2575ff26
Przemek Mroz <frost22a@gmail.com>
2020-09-03 13:49:57 +0200

    upload

---
+++ DayCalculator.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DayOfTheWeekApp.Core
+{
+    public class DayCalculator
+    {
+        public void CalculateDayOfTheWeek(DateTimeOffset date)
+        { 
+            
+        }
+    }
+}
